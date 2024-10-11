532a4f59a7da530446bc19828a77431eb249899b
Omri Levi PC <omrilevi.11@gmail.com>
2024-10-11 15:14:53 +0300

    updated tests

--- tests/renumber.test.ts
+++ tests/renumber.test.ts
@@ -1,37 +1,34 @@
 import { createMockEditor } from "./__mocks__/createMockEditor";
 import Renumberer from "../src/Renumberer";
-import { EditorChange } from "obsidian";
 
 describe("RenumberLocally tests", () => {
-    let changes: EditorChange[];
     let renumberer: Renumberer;
 
     beforeEach(() => {
-        changes = [];
-        renumberer = new Renumberer(changes);
+        renumberer = new Renumberer();
     });
 
     test("No renumbering is done", () => {
         const content = ["1. a", "2. b"];
         const editor = createMockEditor(content);
-        renumberer.addLines(0);
-        const res = renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 0);
+        const res = renumberer.apply(editor, changes);
         expect(res).toBe(false);
     });
 
     test("Renumbering is done", () => {
         const content = ["1. a", "3. b"];
         const editor = createMockEditor(content);
-        renumberer.addLines(0);
-        const res = renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 0);
+        const res = renumberer.apply(editor, changes);
         expect(res).not.toBe(false);
     });
 
     test("Renumber from index 0", () => {
         const content = ["1. a", "3. b"];
         const editor = createMockEditor(content);
-        renumberer.addLines(0);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 0);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", "2. b"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -41,8 +38,8 @@ describe("RenumberLocally tests", () => {
     test("Renumber from the last index", () => {
         const content = ["1. a", "3. b"];
         const editor = createMockEditor(content);
-        renumberer.addLines(1);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 1);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", "2. b"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -52,8 +49,8 @@ describe("RenumberLocally tests", () => {
     test("If previous was not a numbered item, start from current", () => {
         const content = ["A", "1. a", "3. b"];
         const editor = createMockEditor(content);
-        renumberer.addLines(1);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 1);
+        renumberer.apply(editor, changes);
         const expected = ["A", "1. a", "2. b"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -63,8 +60,8 @@ describe("RenumberLocally tests", () => {
     test("A single item", () => {
         const content = ["2. a"];
         const editor = createMockEditor(content);
-        renumberer.addLines(0);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 0);
+        renumberer.apply(editor, changes);
         const expected = ["2. a"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -74,8 +71,8 @@ describe("RenumberLocally tests", () => {
     test("Using the number 0", () => {
         const content = ["0. a", "2. b"];
         const editor = createMockEditor(content);
-        renumberer.addLines(0);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 0);
+        renumberer.apply(editor, changes);
         const expected = ["0. a", "1. b"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -85,8 +82,8 @@ describe("RenumberLocally tests", () => {
     test("Renumber in sequence", () => {
         const content = ["1. a", "6. b", "0. c"];
         const editor = createMockEditor(content);
-        renumberer.addLines(0);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 0);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", "2. b", "3. c"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -96,8 +93,8 @@ describe("RenumberLocally tests", () => {
     test("Renumber according to previous in sequence", () => {
         const content = ["1. a", "6. b", "0. c"];
         const editor = createMockEditor(content);
-        renumberer.addLines(1);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 1);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", "2. b", "3. c"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -107,8 +104,8 @@ describe("RenumberLocally tests", () => {
     test("Stop renumbering at the end of a numbered list", () => {
         const content = ["1. a", "1. b", "A", "5. B"];
         const editor = createMockEditor(content);
-        renumberer.addLines(0);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 0);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", "2. b", "A", "5. B"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -118,8 +115,8 @@ describe("RenumberLocally tests", () => {
     test("Does not modify given a non numbered item", () => {
         const content = ["1. a", "abc", "1. a"];
         const editor = createMockEditor(content);
-        renumberer.addLines(1);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 1);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", "abc", "1. a"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -129,8 +126,8 @@ describe("RenumberLocally tests", () => {
     test("Does not modify if 'x. ' is not in line beginning", () => {
         const content = ["1. a", " 6. A"];
         const editor = createMockEditor(content);
-        renumberer.addLines(1);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 1);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", " 6. A"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -140,8 +137,8 @@ describe("RenumberLocally tests", () => {
     test("Local changes only - begin at index 0, stop at the first correctly numbered item", () => {
         const content = ["1. a", "3. b", "3. c", "5. d"];
         const editor = createMockEditor(content);
-        renumberer.addLines(0);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 0);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", "2. b", "3. c", "5. d"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -151,8 +148,8 @@ describe("RenumberLocally tests", () => {
     test("Local changes only - begin at the middle, stop at the first correctly numbered item", () => {
         const content = ["1. a", "2. b", "3. c", "5. d"];
         const editor = createMockEditor(content);
-        renumberer.addLines(1);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 1);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", "2. b", "3. c", "5. d"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
@@ -162,8 +159,8 @@ describe("RenumberLocally tests", () => {
     test("Local changes only - correct according to previous, stop at the first correctly numbered item", () => {
         const content = ["1. a", "3. b", "3. c", "5. d"];
         const editor = createMockEditor(content);
-        renumberer.addLines(1);
-        renumberer.applyLocal(editor);
+        const { changes } = renumberer.renumberLocally(editor, 1);
+        renumberer.apply(editor, changes);
         const expected = ["1. a", "2. b", "3. c", "5. d"];
         for (let i = 0; i < expected.length; i++) {
             expect(editor.getLine(i)).toBe(expected[i]);
